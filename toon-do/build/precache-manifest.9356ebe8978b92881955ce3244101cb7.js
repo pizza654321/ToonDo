902764a414db8347a02f540b64f57882b0242643
Eugene Li <lieugenebaba01@gmail.com>
2020-04-20 15:13:55 -0400

    uploaded production build

---
+++ toon-do/build/precache-manifest.9356ebe8978b92881955ce3244101cb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f44a862a94ce330dff90e301ac7faec2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "911381320e446f819538",
+    "url": "/static/css/main.bdac26ac.chunk.css"
+  },
+  {
+    "revision": "c3a3ef2b23d8d2a821ff",
+    "url": "/static/js/2.fe9502fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fe9502fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "911381320e446f819538",
+    "url": "/static/js/main.03597415.chunk.js"
+  },
+  {
+    "revision": "d16374566b9bfb910df8",
+    "url": "/static/js/runtime-main.09d27ac4.js"
+  }
+]);
